43972faba3ff52167eacdc5d9d0d241e3a658e08
Guilherme <guila_cid@hotmail.com>
2026-05-16 16:18:21 -0300

    feat: adjust CORS policy at API

--- Frontend/src/shared/components/ui/spinner.tsx
+++ Frontend/src/shared/components/ui/spinner.tsx
@@ -1,8 +1,13 @@
 import { cn } from "@/shared/lib/utils"
-import { HugeiconsIcon } from "@hugeicons/react"
+import { HugeiconsIcon, type HugeiconsIconProps } from "@hugeicons/react"
 import { Loading03Icon } from "@hugeicons/core-free-icons"
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+type SpinnerProps = Omit<
+  HugeiconsIconProps,
+  "icon"
+>;
+
+function Spinner({ className, ...props }: SpinnerProps) {
   return (
     <HugeiconsIcon icon={Loading03Icon} strokeWidth={2} role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
   )
--- ServiceManager/Program.cs
+++ ServiceManager/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173", "http://localhost:4173")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
